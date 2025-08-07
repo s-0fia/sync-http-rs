7dbe13f8d999b0bc279603859d9b15c2d52cdb02
Elle Fleming <ellefleming455@gmail.com>
2025-08-07 17:35:31 +1200

    Improved listener and made it respond with success

--- sync-http/src/lib.rs
+++ sync-http/src/lib.rs
@@ -1,17 +1,46 @@
-use std::{io::Read, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::{Shutdown, TcpListener},
+};
 
 pub mod mime;
 
 pub fn listen() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
 
-    for stream in listener.incoming() {
-        let bytes = stream?.bytes();
-        for byte in bytes {
-            print!("{}", byte? as char);
+    // Read one stream and then terminate
+    let (mut stream, _addr) = listener.accept()?;
+    let mut buf = [0; 128];
+    let mut stop = String::new();
+    'outer: loop {
+        let length = stream.read(&mut buf)?;
+        for b in buf.iter().take(length) {
+            let ch = *b as char;
+            print!("{}", ch);
+            match ch {
+                '\r' => stop.push(ch),
+                '\n' => {
+                    if !stop.is_empty() {
+                        stop.push(ch)
+                    }
+                }
+                _ => {
+                    if !stop.is_empty() {
+                        stop = String::new()
+                    }
+                }
+            }
+            if stop.contains("\r\n\r\n") {
+                break 'outer;
+            }
         }
-        println!();
     }
+    stream.shutdown(Shutdown::Read)?;
+
+    let response = b"HTTP/1.1 200\r\n\r\n";
+    stream.write_all(response)?;
+
+    stream.shutdown(Shutdown::Write)?;
 
     Ok(())
 }
