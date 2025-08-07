b68b0e788a3ea43e4be021dca0382ee24b1890bc
Elle Fleming <ellefleming455@gmail.com>
2025-08-07 18:34:59 +1200

    Made tests not compile if new mime types are added

--- sync-http/src/mime.rs
+++ sync-http/src/mime.rs
@@ -118,6 +118,26 @@ impl ContentType {
 #[cfg(test)]
 mod tests {
     use super::*;
+    macro_rules! check_new_mimes {
+        ($v:expr) => {
+            match $v {
+                MimeType::All
+                | MimeType::JSON
+                | MimeType::OctetStream
+                | MimeType::APNG
+                | MimeType::AVIF
+                | MimeType::GIF
+                | MimeType::JPEG
+                | MimeType::PNG
+                | MimeType::SVG
+                | MimeType::Webp
+                | MimeType::CSS
+                | MimeType::HTML
+                | MimeType::Javascript
+                | MimeType::Plain => {}
+            }
+        };
+    }
 
     #[test]
     fn parse_all_types() {
@@ -136,6 +156,10 @@ mod tests {
         const NUM_TYPES: usize = 2;
         let app_types: [_; NUM_TYPES] = ["octet-stream", "json"];
         let mime_types: [_; NUM_TYPES] = [MimeType::OctetStream, MimeType::JSON];
+
+        // Will cause error if more mime types are added
+        check_new_mimes!(mime_types[0]);
+
         for i in 0..NUM_TYPES {
             let ct = ContentType::parse(format!("application/{}", app_types[i]));
             assert_eq!(ct, Some(ContentType(MediaType::Application, mime_types[i])));
@@ -155,6 +179,10 @@ mod tests {
             MimeType::SVG,
             MimeType::Webp,
         ];
+
+        // Will cause error if more mime types are added
+        check_new_mimes!(mime_types[0]);
+
         for i in 0..NUM_TYPES {
             let ct = ContentType::parse(format!("image/{}", image_types[i]));
             assert_eq!(ct, Some(ContentType(MediaType::Image, mime_types[i])));
@@ -171,6 +199,10 @@ mod tests {
             MimeType::Javascript,
             MimeType::Plain,
         ];
+
+        // Will cause error if more mime types are added
+        check_new_mimes!(mime_types[0]);
+
         for i in 0..NUM_TYPES {
             let ct = ContentType::parse(format!("text/{}", text_types[i]));
             assert_eq!(ct, Some(ContentType(MediaType::Text, mime_types[i])));
