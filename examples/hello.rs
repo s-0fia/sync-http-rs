f5c82c9bc4bcc18d366842698f2a41baa41bfd4a
Elle Fleming <ellefleming455@gmail.com>
2025-08-08 22:38:45 +1200

    Cleaned up examples

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,26 +1,14 @@
-use std::{error::Error, net::TcpStream};
-
-use sync_http::{
-    query::Query,
-    server::{Server, ServerStream},
-};
+use sync_http::{query::Query, server::Server, ServerResult};
 
 fn main() {
-    let mut serv = Server::create()
+    Server::create()
         .ip_address("192.168.126.128".into())
         .get("/", &index)
-        .get("/close?", &shutdown)
         .bind()
         .unwrap();
-
-    loop {
-        let (mut stream, req) = serv.request().unwrap();
-        dbg!(&req);
-        serv.handle_get(&mut stream, req).unwrap();
-    }
 }
 
-fn index(_query: Query) -> Result<String, Box<dyn Error>> {
+fn index(_query: Query) -> ServerResult<String> {
     Ok(r#"<html>
         <head>
             <title>Hello, world!</title>
@@ -31,19 +19,6 @@ fn index(_query: Query) -> Result<String, Box<dyn Error>> {
             <form action="./close">
                 <input type="submit" value="Shut Server Down" />
             </form>
-        </body>
-        </html>
-        "#
-    .to_string())
-}
-
-fn shutdown(_query: Query) -> Result<String, Box<dyn Error>> {
-    Ok(r#"<html>
-        <head>
-            <title>Shutting down</title>
-        </head>
-        <body>
-            <h1>Shutting the server down</h1>
         </body>
         </html>
         "#
---
+++ examples/shutdown.rs
@@ -0,0 +1,40 @@
+use std::{
+    sync::mpsc::{channel, Sender},
+    thread,
+    time::Duration,
+};
+use sync_http::{query::Query, server::Server, ServerResult};
+
+static mut SHUTDOWN: Option<Sender<()>> = None;
+
+fn main() {
+    let (send, recv) = channel();
+    unsafe {
+        SHUTDOWN = Some(send);
+    }
+    Server::create()
+        .ip_address("192.168.126.128".into())
+        .get("/", &shutdown)
+        .shutdown(recv)
+        .bind()
+        .unwrap();
+}
+
+fn shutdown(_query: Query) -> ServerResult<String> {
+    if let Some(shutdown) = unsafe { SHUTDOWN.clone() } {
+        shutdown.send(())?;
+        // Sleep 100 ms just to ensure that the
+        // thread recieves the shutdown signal
+        thread::sleep(Duration::from_millis(100));
+    }
+    Ok(r#"<html>
+        <head>
+            <title>Shutting down</title>
+        </head>
+        <body>
+            <h1>Shutting the server down</h1>
+        </body>
+        </html>
+        "#
+    .to_string())
+}
