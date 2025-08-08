d4ba495690f7beccdd251162c8c45525c5a7ce98
Elle Fleming <ellefleming455@gmail.com>
2025-08-08 21:24:24 +1200

    Removed old code

--- sync-http/src/lib.rs
+++ sync-http/src/lib.rs
@@ -8,33 +8,3 @@ pub mod errors;
 pub mod mime;
 pub mod request;
 pub mod server;
-
-pub fn listen() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
-
-    // Read one stream and then terminate
-    let (mut stream, _addr) = listener.accept()?;
-    let mut buf = [0; 128];
-    let mut request = String::new();
-    loop {
-        let length = stream.read(&mut buf)?;
-        for b in buf.iter().take(length) {
-            let ch = *b as char;
-            request.push(ch);
-        }
-        if request.contains("\r\n\r\n") {
-            break;
-        }
-    }
-    println!("{request}");
-    let request = Request::parse(request);
-    dbg!(&request);
-    stream.shutdown(Shutdown::Read)?;
-
-    let response = b"HTTP/1.1 200\r\n\r\n";
-    stream.write_all(response)?;
-
-    stream.shutdown(Shutdown::Write)?;
-
-    Ok(())
-}
