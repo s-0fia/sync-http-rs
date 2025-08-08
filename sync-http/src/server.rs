b1fe48677eeb8533c499d67188a542ffd4382ee4
Elle Fleming <ellefleming455@gmail.com>
2025-08-08 17:28:36 +1200

    Implemented an empty response function

--- sync-http/src/server.rs
+++ sync-http/src/server.rs
@@ -1,7 +1,7 @@
 use std::{
     error::Error,
     io::{self, Read, Write},
-    net::{Shutdown, TcpListener},
+    net::{Shutdown, TcpListener, TcpStream},
 };
 
 use crate::request::Request;
@@ -12,15 +12,17 @@ pub struct ServerBuilder {
     ttl: Option<u32>,
 }
 
+#[derive(Debug)]
 pub struct Server {
     listener: TcpListener,
+    expecting_response: bool,
 }
 
 impl Server {
     /// Creates a new ServerBuilder which defaults with values of:
     /// - ttl: None (time to live)
     /// - ip_address: 127.0.0.1
-    /// - port: 80
+    /// - port: 8080
     pub fn create() -> ServerBuilder {
         ServerBuilder {
             ip_address: None,
@@ -29,7 +31,7 @@ impl Server {
         }
     }
 
-    pub fn get_request(&self) -> Result<Request, Box<dyn Error>> {
+    pub fn get_request(&mut self) -> Result<(TcpStream, Request), Box<dyn Error>> {
         let (mut stream, _addr) = self.listener.accept()?;
         let mut buf = [0; 128];
         let mut request = String::new();
@@ -45,7 +47,22 @@ impl Server {
         }
         let request = Request::parse(request)?;
         stream.shutdown(Shutdown::Read)?;
-        Ok(request)
+        self.expecting_response = true;
+        Ok((stream, request))
+    }
+
+    pub fn close_response(&mut self, stream: &TcpStream) -> Result<(), Box<dyn Error>> {
+        if self.expecting_response {
+            stream.shutdown(Shutdown::Write)?;
+            self.expecting_response = false;
+        }
+        Ok(())
+    }
+
+    pub fn write_empty(&mut self, stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
+        let response = b"HTTP/1.1 200\r\n\r\n";
+        stream.write_all(response)?;
+        self.close_response(stream)
     }
 }
 
@@ -67,13 +84,17 @@ impl ServerBuilder {
 
     pub fn bind(self) -> Result<Server, io::Error> {
         let ip = self.ip_address.unwrap_or("127.0.0.1".into());
-        let port = self.port.unwrap_or(80);
-        let listener = TcpListener::bind(format!("{ip}:{port}"))?;
+        let port = self.port.unwrap_or(8080);
+        let addr = format!("{ip}:{port}");
+        let listener = TcpListener::bind(addr)?;
 
         if let Some(ttl) = self.ttl {
             listener.set_ttl(ttl)?;
         }
 
-        Ok(Server { listener })
+        Ok(Server {
+            listener,
+            expecting_response: false,
+        })
     }
 }
