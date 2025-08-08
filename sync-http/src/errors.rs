3779bc748a18bcc372815c71ffe3027f36f85aa1
Elle Fleming <ellefleming455@gmail.com>
2025-08-08 17:06:30 +1200

    Made RequestError implement Error for easier unwrapping

--- sync-http/src/errors.rs
+++ sync-http/src/errors.rs
@@ -1,6 +1,16 @@
+use std::{error::Error, fmt::Display};
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum RequestError {
     TooManyValues,
     BadMethod,
     BadProtocol,
 }
+
+impl Display for RequestError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl Error for RequestError {}
