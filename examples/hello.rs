68bd6f0dc68ee2f63accccc1a4d2c054ffc04e86
Elle Fleming <ellefleming455@gmail.com>
2025-08-08 17:45:51 +1200

    Improved hello.rs example

--- examples/hello.rs
+++ examples/hello.rs
@@ -5,5 +5,16 @@ fn main() {
 
     let (mut stream, req) = serv.get_request().unwrap();
     dbg!(req);
-    serv.write_empty(&mut stream);
+    let response = r#"
+    <html>
+    <head>
+        <title> Hello, world!</title>
+    </head>
+    <body>
+        <h1>Hello, world!</h1>
+        <p>Example webpage</p>
+    </body>
+    </html>
+    "#;
+    serv.write_all(&mut stream, response.as_bytes()).unwrap();
 }
